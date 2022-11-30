6d71835a0feb35e8f5f393df5abcd0322d25c827
Devany15 <76234922+Devany15@users.noreply.github.com>
2022-11-29 22:15:00 -0600

    SQLite

--- app/src/main/java/com/example/pia/MainActivity.kt
+++ app/src/main/java/com/example/pia/MainActivity.kt
@@ -4,13 +4,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import androidx.appcompat.app.AppCompatViewInflater
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 
     fun RegistroOnClick(view: View){
